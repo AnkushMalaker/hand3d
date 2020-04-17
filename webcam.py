0292ed2cb1d136945994c4611683529199c556d1
AnkushMalaker <ankushmalaker2000@gmail.com>
2020-04-17 14:08:56 +0530

    Added webcam support

---
+++ webcam.py
@@ -0,0 +1,106 @@
+#
+#  ColorHandPose3DNetwork - Network for estimating 3D Hand Pose from a single RGB Image
+#  Copyright (C) 2017  Christian Zimmermann
+#  
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 2 of the License, or
+#  (at your option) any later version.
+#  
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#  
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+from __future__ import print_function, unicode_literals
+
+
+from tensorflow.compat.v1 import ConfigProto
+from tensorflow.compat.v1 import InteractiveSession
+
+config = ConfigProto()
+config.gpu_options.allow_growth = True
+session = InteractiveSession(config=config)
+
+
+import tensorflow as tf
+import numpy as np
+import scipy.misc
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+import cv2
+import time
+from nets.ColorHandPose3DNetwork import ColorHandPose3DNetwork
+from utils.general import detect_keypoints, trafo_coords, plot_hand, plot_hand_3d
+import io
+
+def get_img_from_fig(fig, dpi=180):
+    buf = io.BytesIO()
+    fig.savefig(buf, format="png", dpi=dpi)
+    buf.seek(0)
+    img_arr = np.frombuffer(buf.getvalue(), dtype=np.uint8)
+    buf.close()
+    img = cv2.imdecode(img_arr, 1)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+    return img
+
+if __name__ == '__main__':
+    # images to be shown
+    cap = cv2.VideoCapture(0)
+    fps = cap.get(cv2.CAP_PROP_FPS)
+    print("Frames per second using video.get(cv2.CAP_PROP_FPS) : {0}".format(fps))
+
+    if not cap.isOpened():
+        raise IOError("Cannot open webcam")
+
+    # network input
+    image_tf = tf.placeholder(tf.float32, shape=(1, 240, 320, 3))
+    hand_side_tf = tf.constant([[1.0, 0.0]])  # left hand (true for all samples provided)
+    evaluation = tf.placeholder_with_default(True, shape=())
+
+    # build network
+    net = ColorHandPose3DNetwork()
+    hand_scoremap_tf, image_crop_tf, scale_tf, center_tf,\
+    keypoints_scoremap_tf, keypoint_coord3d_tf = net.inference(image_tf, hand_side_tf, evaluation)
+
+    # Start TF
+    gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.8)
+    sess = tf.Session(config=tf.ConfigProto(gpu_options=gpu_options))
+
+    # initialize network
+    net.init(sess)
+    while True:
+        ret, frame = cap.read()
+        frame = cv2.resize(frame, None, fx=0.5, fy=0.5, interpolation=cv2.INTER_AREA)
+        image_raw = frame
+        image_raw = scipy.misc.imresize(image_raw, (240, 320))
+        image_v = np.expand_dims((image_raw.astype('float') / 255.0) - 0.5, 0)
+        print(image_v.shape)
+        hand_scoremap_v, image_crop_v, scale_v, center_v,\
+        keypoints_scoremap_v, keypoint_coord3d_v = sess.run([hand_scoremap_tf, image_crop_tf, scale_tf, center_tf,
+                                                             keypoints_scoremap_tf, keypoint_coord3d_tf],
+                                                            feed_dict={image_tf: image_v})
+
+        hand_scoremap_v = np.squeeze(hand_scoremap_v)
+        image_crop_v = np.squeeze(image_crop_v)
+        keypoints_scoremap_v = np.squeeze(keypoints_scoremap_v)
+        keypoint_coord3d_v = np.squeeze(keypoint_coord3d_v)
+
+        # post processing
+        image_crop_v = ((image_crop_v + 0.5) * 255).astype('uint8')
+        coord_hw_crop = detect_keypoints(np.squeeze(keypoints_scoremap_v))
+        coord_hw = trafo_coords(coord_hw_crop, center_v, scale_v, 256)
+
+       
+        
+        cv2.imshow('Output', frame)
+        c = cv2.waitKey(1)
+        if c == 27:
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
